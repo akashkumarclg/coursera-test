3fe06e09e23c28ddf9c2bfe13df5491f668593f5
akash kumar <akashkumarclg@gmail.com>
2018-09-30 16:41:03 +0530

    module 4 assignment

---
+++ module4/js/script.js
@@ -0,0 +1,18 @@
+(function () {
+
+var names = ["Akash", "John", "Jen", "Jay", "Paul", "Ashish", "Larry", "Sumit", "Piyush", "Joy"];
+
+
+for (var i = 0; i < names.length; i++) {
+   
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+    
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
